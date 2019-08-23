0fa1b1c3a9d94fd47ac5927c6b508081be52b466
wolframowsky <wolframowsky@gmail.com>
2019-08-24 00:55:17 +0200

    post view in progress

---
+++ petogram/petogram/Controllers/PostController.cs
@@ -0,0 +1,33 @@
+﻿using petogram.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace petogram.Controllers
+{
+    public class PostController : Controller
+    {
+        // GET: Post
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Post model)
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
